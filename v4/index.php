0c527e1e5072c1f94c9f8d1312d354089035ad5b
Ivan Pisquiy <pisquiy22@gmail.com>
2023-06-08 21:14:54 -0600

    Centering footer

--- v4/index.php
+++ v4/index.php
@@ -720,7 +720,7 @@
     <footer>
         <div class="container">
             <div class="solar-footer-content">
-                <div class="row">
+                <div class="row footer-row">
                     <div class="footer-menu">
                         <div id="footer" class="form-group">
                             <p id="tcpa_label"> Copyright&copy; 2023&nbsp;poweredbysolar.energy.&nbsp;All Rights Reserved &nbsp;<a href="privacy.php" target="_new">Privacy policy</a></p>
