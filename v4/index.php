b6720f7008d505ce38ca181ffdb5f432a3a82414
Ivan Pisquiy <pisquiy22@gmail.com>
2023-06-08 21:03:52 -0600

    Removing Terms and Conditions

--- v4/index.php
+++ v4/index.php
@@ -723,7 +723,7 @@
                 <div class="row">
                     <div class="footer-menu">
                         <div id="footer" class="form-group">
-                            <p id="tcpa_label"> Copyright&copy; 2023&nbsp;poweredbysolar.energy.&nbsp;All Rights Reserved &nbsp;<a href="privacy.php" target="_new">Privacy policy</a> || <a href="terms.php" target="_new">Terms And Conditions</a> </p>
+                            <p id="tcpa_label"> Copyright&copy; 2023&nbsp;poweredbysolar.energy.&nbsp;All Rights Reserved &nbsp;<a href="privacy.php" target="_new">Privacy policy</a></p>
                         </div>
                     </div>
                 </div>
